127535aa07a96833b1f57b442e9a6679ca0cd511
Erik <erik.chau@gmail.com>
2014-08-06 19:33:51 -0700

    test

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var http = require('http'),
     static = require('node-static');
 
 var file = new static.Server('./public');
-
+var port = process.env.PORT || 8080;
 var server = http.createServer(function (req, res) {
   req.addListener('end', function () {
     file.serve(req, res);
   }).resume();
 });
 
-server.listen(8000);
+server.listen(port);
 
 chatServer.listen(server);
